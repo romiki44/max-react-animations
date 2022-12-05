337ef570aad885960c066557593720a6c2b43650
RomanK <romiki44@hotmail.com>
2022-12-05 14:50:47 +0100

    sec24-357 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -6,7 +7,7 @@ import Backdrop from './components/Backdrop/Backdrop';
 import List from './components/List/List';
 
 class App extends Component {
-  state = { modalIsOpen: false };
+  state = { modalIsOpen: false, showBlock: false };
 
   showModal = () => {
     this.setState({ modalIsOpen: true });
@@ -16,7 +17,71 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   };
 
+  toggleBlock = () => {
+    this.setState((prevState) => ({ showBlock: !prevState.showBlock }));
+  };
+
+  redSquare = (
+    <div
+      style={{
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+        margin: 'auto',
+      }}
+    ></div>
+  );
+
   render() {
+    return (
+      <div className='App'>
+        <h1>React Animations</h1>
+        <button className='Button' onClick={this.showModal}>
+          Open Modal
+        </button>
+        <br />
+        <button className='Button' onClick={this.toggleBlock}>
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {/* {(state) => <p>{state}</p>} */}
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s easy-out',
+                opacity: state === 'exiting' ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+
+  //pouzitie css-transitions a css-animations...vid Backdrop.css a Modal.css
+  //musel som to ale zakomentovat, lebo s tym Toggle to uz potom vobec nechodi
+  /*render() {
     return (
       <div className='App'>
         <h1>React Animations</h1>
@@ -29,7 +94,7 @@ class App extends Component {
         <List />
       </div>
     );
-  }
+  }*/
 }
 
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+  const cssClasses = [
+    'Modal',
+    props.show == 'entering'
+      ? 'ModalOpen'
+      : props.show === 'exiting'
+      ? 'ModalClose'
+      : null,
+  ];
   return (
     <div className={cssClasses.join(' ')}>
       <h1>A Modal</h1>
@@ -13,5 +20,17 @@ const modal = (props) => {
     </div>
   );
 };
+
+/*const modal = (props) => {
+  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+  return (
+    <div className={cssClasses.join(' ')}>
+      <h1>A Modal</h1>
+      <button className='Button' onClick={props.closed}>
+        Dismiss
+      </button>
+    </div>
+  );
+};*/
 
 export default modal;
